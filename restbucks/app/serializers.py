665cb9e299d40030a03916c641ee1b6432f39496
Mohammad Reza <mohammadreza.rahmani89@gmail.com>
2019-09-24 19:04:45 +0330

    View user orders end-point added

--- restbucks/app/serializers.py
+++ restbucks/app/serializers.py
@@ -1,6 +1,7 @@
+from django.db.models import Sum, F
 from rest_framework import serializers
 
-from app.models import Product, Order
+from app.models import Product, Order, OrderItem
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -9,7 +10,39 @@ class ProductSerializer(serializers.ModelSerializer):
 		exclude = ['created_at', 'last_modified', 'slug']
 
 
+class OrderItemSerializer(serializers.ModelSerializer):
+	options = serializers.SerializerMethodField()
+	item_price = serializers.SerializerMethodField()
+	product = ProductSerializer()
+
+	class Meta:
+		model = OrderItem
+		fields = ['product', 'options', 'count', 'item_price']
+
+	def get_options(self, order_item):
+		options = {}
+		for field in OrderItem.customization_options.keys():
+			value = getattr(order_item, field, None)
+			if value:
+				options.update({field: value})
+		return options
+
+	def get_item_price(self, order_item):
+		return order_item.count * order_item.product.price
+
+
 class OrderSerializer(serializers.ModelSerializer):
+	customer = serializers.StringRelatedField()
+	total_price = serializers.SerializerMethodField()
+	products = OrderItemSerializer(many=True, read_only=True, source='order_item_set')
+
 	class Meta:
 		model = Order
-		fields = '__all__'
+		exclude = ['last_modified']
+
+	def get_total_price(self, order):
+		return order.order_item_set.annotate(
+			item_price=F('product__price') * F('count')
+		).aggregate(
+			total_price=Sum('item_price')
+		).get('total_price')
