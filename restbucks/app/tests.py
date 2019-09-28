928099934baaed46c3a3437a51207ca66b549586
Mohammad Reza <mohammadreza.rahmani89@gmail.com>
2019-09-28 23:44:52 +0330

    Added order model tests

--- restbucks/app/tests.py
+++ restbucks/app/tests.py
@@ -1,27 +1,22 @@
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.test import TestCase
-from django.utils import timezone
 
-from app.models import Product
+from app.models import Product, Order, OrderItem
 
 
 class ProductModelTests(TestCase):
-	def test_product_creation(self):
+	def test_creation(self):
 		product = Product.objects.create(name='Hot Chocolate', price=1000)
 
 		# check if created_at, last_modified and slug are set
-		self.assertIsNotNone(product.created_at)
-		self.assertIsNotNone(product.last_modified)
-		self.assertIsNotNone(product.slug)
-
-		# check if created_at and last_modified are valid
-		now = timezone.now()
-		self.assertLess(product.created_at, now)
-		self.assertLess(product.last_modified, now)
+		for field in ['created_at', 'last_modified', 'slug']:
+			self.assertIsNotNone(getattr(product, field, None))
 
 		# check if slug is valid
 		self.assertEqual(product.slug, 'hot-chocolate')
 
-	def test_product_update(self):
+	def test_update(self):
 		product = Product.objects.create(name='Hot Chocolate', price=1000)
 
 		# update product
@@ -31,8 +26,54 @@ class ProductModelTests(TestCase):
 		# check if slug has been updated
 		self.assertEqual(product.slug, 'latte')
 
-		# TODO: needs completion
-		now = timezone.now()
-		last_modified_diff = (now - product.last_modified).seconds
-		self.assertLess(last_modified_diff, 1)
-		created_at_diff = (now - product.created_at).seconds
+
+class OrderModelTests(TestCase):
+	fixtures = ['product.json']
+
+	def setUp(self):
+		self.user = User.objects.create_user('test')
+
+	def test_creation(self):
+		order = Order.objects.create(customer=self.user)
+		for i in range(1, 4):
+			OrderItem.objects.create(order=order, product_id=i)
+
+		# check if items added
+		self.assertEqual(order.items.count(), 3)
+
+	def test_delivery_method(self):
+		order = Order(customer=self.user, delivery_method=Order.TAKE_AWAY)
+
+		# check if address is mandatory for take away order
+		try:
+			order.full_clean()
+			self.fail('Oops')
+		except ValidationError as e:
+			self.assertIn('delivery_address', e.message_dict)
+
+	def test_price(self):
+		order, total_price = Order.objects.create(customer=self.user), 0
+		for i in range(1, 4):
+			order_item = OrderItem.objects.create(order=order, product_id=i, count=i)
+			total_price += i * order_item.product.price
+
+		# check if order price is correct
+		self.assertEqual(order.price, total_price)
+
+
+class OrderItemModelTests(TestCase):
+	fixtures = ['product.json']
+
+	def setUp(self):
+		self.user = User.objects.create_user('test')
+
+	def test_creation(self):
+		pass
+
+	def test_options(self):
+		# check default
+		# check validation
+		pass
+
+	def test_price(self):
+		pass
