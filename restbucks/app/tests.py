f52cd111eee269744d2254e246b3c5b077c72e28
Mohammad Reza <mohammadreza.rahmani89@gmail.com>
2019-09-28 13:55:43 +0330

    Product creation test added

--- restbucks/app/tests.py
+++ restbucks/app/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.utils import timezone
 
-# Create your tests here.
+from app.models import Product
+
+
+class ProductModelTests(TestCase):
+	def test_product_creation(self):
+		product = Product.objects.create(name='Hot Chocolate', price=100)
+
+		# check if created_at, last_modified and slug are set
+		self.assertIsNotNone(product.created_at)
+		self.assertIsNotNone(product.last_modified)
+		self.assertIsNotNone(product.slug)
+
+		# check if created_at and last_modified are valid
+		now = timezone.now()
+		self.assertLess(product.created_at, now)
+		self.assertLess(product.last_modified, now)
+
+		# check if slug is valid
+		self.assertEqual(product.slug, 'hot-chocolate')
