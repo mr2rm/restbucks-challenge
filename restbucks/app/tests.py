85718fec996a2cc2804ff6eed8aa5f0552045a6a
Mohammad Reza <mohammadreza.rahmani89@gmail.com>
2019-09-28 15:37:51 +0330

    Add another test for product

--- restbucks/app/tests.py
+++ restbucks/app/tests.py
@@ -6,7 +6,7 @@ from app.models import Product
 
 class ProductModelTests(TestCase):
 	def test_product_creation(self):
-		product = Product.objects.create(name='Hot Chocolate', price=100)
+		product = Product.objects.create(name='Hot Chocolate', price=1000)
 
 		# check if created_at, last_modified and slug are set
 		self.assertIsNotNone(product.created_at)
@@ -20,3 +20,19 @@ class ProductModelTests(TestCase):
 
 		# check if slug is valid
 		self.assertEqual(product.slug, 'hot-chocolate')
+
+	def test_product_update(self):
+		product = Product.objects.create(name='Hot Chocolate', price=1000)
+
+		# update product
+		product.name = 'Latte'
+		product.save()
+
+		# check if slug has been updated
+		self.assertEqual(product.slug, 'latte')
+
+		# TODO: needs completion
+		now = timezone.now()
+		last_modified_diff = (now - product.last_modified).seconds
+		self.assertLess(last_modified_diff, 1)
+		created_at_diff = (now - product.created_at).seconds
